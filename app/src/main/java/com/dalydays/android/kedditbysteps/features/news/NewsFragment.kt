1d991c8f931fb99e3f9db49462b603bb74cbdfc0
linucksrox <ericdalyblog@dalydays.com>
2018-03-20 06:35:56 -0400

    Add Kotlin synthetic properties to NewsFragment (from part 3)

--- app/src/main/java/com/dalydays/android/kedditbysteps/features/news/NewsFragment.kt
+++ app/src/main/java/com/dalydays/android/kedditbysteps/features/news/NewsFragment.kt
@@ -3,12 +3,12 @@ package com.dalydays.android.kedditbysteps.features.news
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dalydays.android.kedditbysteps.R
 import com.dalydays.android.kedditbysteps.commons.inflate
+import kotlinx.android.synthetic.main.news_fragment.*
 
 /**
  * Created by edaly on 3/19/2018.
@@ -16,14 +16,14 @@ import com.dalydays.android.kedditbysteps.commons.inflate
 
 class NewsFragment: Fragment() {
 
-    private var newsList: RecyclerView? = null
-
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 //        val view = inflater?.inflate(R.layout.news_fragment, container, false)
+        // using Extensions.kt, we can avoid using the line above, and instead use the line below
         val view = container?.inflate(R.layout.news_fragment)
-        newsList = view?.findViewById(R.id.news_list) as RecyclerView?
-        newsList?.setHasFixedSize(true)
-        newsList?.layoutManager = LinearLayoutManager(context)
+
+        // using Kotlin synthetic properties, directly access XML elements by ID
+        news_list.setHasFixedSize(true)
+        news_list.layoutManager = LinearLayoutManager(context)
 
         return view
     }
